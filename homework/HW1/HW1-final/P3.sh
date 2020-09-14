98ba75f7dea9dca9cb1c8a419528808ea045b93e
Benjamin Manning <65459344+berzerkerBM@users.noreply.github.com>
2020-09-14 19:30:50 -0400

    homework P3 fix

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -2,5 +2,5 @@
 grep "[0-9]" apollo13.txt | wc -l > apollo_out.txt
 grep --help | grep -- --count
 ls -l | grep -c "\.py$"
-ls -lR | grep "\-......\-\-." | wc -l
-ls -l | grep ".......\-\-." | wc -l
+ls -lR | grep "^\-......\-\-." | wc -l
+ls -l | grep "^\-*d*.......\-\-." | wc -l
