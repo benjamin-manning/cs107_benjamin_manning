3881bd5035701f8b85f30ea88c17a258ee66ac98
Benjamin Manning <65459344+berzerkerBM@users.noreply.github.com>
2020-10-29 17:53:18 -0400

    pset 5 start

---
+++ homework/HW5/animal.py
@@ -0,0 +1,45 @@
+class Animal:
+
+    # a class attribute of the valid species in our universe
+    valid_species = {
+        'cat',
+        'dog',
+        'duck',
+        'elf',
+        'goblin',
+        'horse',
+        'human',
+        'mermaid',
+        'nightingale',
+        'pig',
+        'swan',
+        'wolf'
+    }
+
+    def __init__(self, name, species):
+        self.name = name
+        self._species = species
+    
+    @property
+    def species(self):
+        return self._species 
+    
+    @species.setter
+    def species(self,into):
+        if into in self.valid_species:
+            self._species = into
+            return self._species 
+        else:
+            raise Exception('Not a valid species')
+
+
+    def __repr__(self):
+        return f'{self.name} ({self._species})'
+
+dog = Animal('Fido', 'dog')
+print(dog.species)
+
+dog.species = 'cat'
+print(dog.species)
+
+dog.species = 'TheThing'
