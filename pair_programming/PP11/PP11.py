6a361c7215b72e5b9829e30ac1d539121e97a82e
Benjamin Manning <65459344+berzerkerBM@users.noreply.github.com>
2020-11-16 17:13:21 -0500

    adding PP11 files

---
+++ pair_programming/PP11/PP11.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+# Ben Manning
+# Nicole Araya
+# Qinyi Chen
+
+db = sqlite3.connect('candidates.sqlite')
+cursor = db.cursor() # https://www.python.org/dev/peps/pep-0249/#cursor-objects
+cursor.execute("DROP TABLE IF EXISTS candidates") # Convenient in case you want to start over
+cursor.execute("DROP TABLE IF EXISTS contributors") # Convenient in case you want to start over
+print(cursor)
+
+cursor.execute('''CREATE TABLE candidates (
+               id TEXT, 
+               first_name TEXT, 
+               last_name TEXT, 
+               middle_name TEXT, 
+               party TEXT NOT NULL)''')
+
+db.commit() # Commit changes to the database
+
+a_file = open("candidates.txt", "r")
+candidates = []
+lines = a_file.readlines()
+for line in lines[1:]:
+    line = line.strip('\n')
+    line = tuple(line.split('|'))
+    candidates.append(line)
+
+cursor.executemany('INSERT INTO candidates (id, first_name, last_name, middle_name, party) VALUES (?, ?, ?, ?, ?)', candidates)
+db.commit()
+
+
+cursor.execute("SELECT * FROM candidates")
+all_rows = cursor.fetchall()
+print(all_rows)
